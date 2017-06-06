131595f35a6479d7b22ff2526fad5e0a69ac1363
Tim Gallant <tgallant@attn.com>
2017-06-05 22:35:53 -0700

    adds rollup and postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,67 @@
+'use strict'
+
+const autoprefixer = require('autoprefixer')
+const babel = require('rollup-plugin-babel')
+const concat = require('gulp-concat')
 const connect = require('gulp-connect')
+const cssnano = require('cssnano')
 const gulp = require('gulp')
 const panini = require('panini')
+const postcss = require('gulp-postcss')
+const resolve = require('rollup-plugin-node-resolve')
+const rollup = require('rollup').rollup
+const sourcemaps = require('gulp-sourcemaps')
+const uglify = require('rollup-plugin-uglify')
 
 gulp.task('html', () => {
   const paniniOptions = {
-    root: 'pages/',
-    layouts: 'layouts/',
-    partials: 'partials/',
-    helpers: 'helpers/',
-    data: 'data/'
+    root: 'src/pages/',
+    layouts: 'src/layouts/',
+    partials: 'src/partials/',
+    helpers: 'src/helpers/',
+    data: 'src/data/'
   }
 
-  gulp.src('pages/**/*.html')
+  gulp.src('src/pages/**/*.html')
     .pipe(panini(paniniOptions))
     .pipe(gulp.dest('build'))
 })
 
 gulp.task('js', () => {
-  return gulp.src('js/**/*.js', { base: '.' })
-    .pipe(gulp.dest('build'))
+  return rollup({
+    entry: 'assets/js/index.js',
+    plugins: [
+      resolve({
+        browser: true,
+        jsnext: true,
+        module: true
+      }),
+      babel({
+        exclude: 'node_modules'
+      }),
+      uglify()
+    ]
+  }).then(bundle => {
+    return bundle.write({
+      format: 'iife',
+      dest: 'build/js/main.js',
+      sourceMap: true
+    })
+  })
 })
 
 gulp.task('css', () => {
-  return gulp.src('css/**/*.css', { base: '.' })
-    .pipe(gulp.dest('build'))
+  const plugins = [
+    autoprefixer(),
+    cssnano()
+  ]
+
+  return gulp.src(['assets/css/reset.css', 'assets/css/**/*.css'])
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.css'))
+    .pipe(postcss(plugins))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/css'))
 })
 
 gulp.task('assets', ['js', 'css'])
@@ -36,13 +74,13 @@ gulp.task('watch', ['default'], () => {
     port: 8888
   })
 
-  gulp.watch(['js/**/*.js'], ['js'])
-  gulp.watch(['css/**/*.css'], ['css'])
+  gulp.watch(['assets/js/**/*.js'], ['js'])
+  gulp.watch(['assets/css/**/*.css'], ['css'])
   gulp.watch([
-    'pages/**/*',
-    'partials/**/*',
-    'helpers/**/*',
-    'data/**/*',
-    'layouts/**/*'
+    'src/pages/**/*',
+    'src/partials/**/*',
+    'src/helpers/**/*',
+    'src/data/**/*',
+    'src/layouts/**/*'
   ], ['html'])
 })
